a914d95bc9f4200bbbe4aefff83aa2f018febbcb
tdunn1248 <tdunn1248@gmail.com>
2017-07-31 20:24:28 -0700

    setup database connection

--- part-1/app.js
+++ part-1/app.js
@@ -5,21 +5,23 @@ const port = 3008
 
 const daysOfWeek = {monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Simple Web App</h1>')
 })
 
 app.get('/api/days/:day', (request, response) => {
-  if (!daysOfWeek.hasOwnProperty(request.params.day)) {
-    response.set('Content-Type', 'application/text')
-    response.status(400).send(request.params.day + ' is not a valid day!')
+  /*
+  When the parameter is an Array or Object, Express responds
+  with the JSON representation:
+  ask punit
+  */
+  response.set('Content-Type', 'text/html')
+  if (daysOfWeek.hasOwnProperty(request.params.day)) {
+    const dayNumber = daysOfWeek[request.params.day]
+    response.status(200).send({response: dayNumber})
   } else {
-    response.set('Content-Type', 'application/text')
-    var dayNum = daysOfWeek[request.params.day]
-    response.status(200).send({response: dayNum})
+    response.json({error: request.params.day + ' is not a valid day'})
   }
 })
 
@@ -38,7 +40,6 @@ function arrayFormattedCorrectly(array) {
 app.post('/api/arrays/concat', (request, response) => {
   const string1 = request.body.string1
   const string2 = request.body.string2
-
   if(arrayFormattedCorrectly(string1) === false || arrayFormattedCorrectly(string2) === false) {
     response.status(400).json({error: 'Input data should be type Array'})
   }
